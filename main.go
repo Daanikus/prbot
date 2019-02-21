9b4492fbdf56535f16454270d36721940061bdb7
Dan Bent <daanikus@gmail.com>
2019-02-21 17:33:31 +1300

    Begin adding auto-PR functionality

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ import (
 	"github.com/google/go-github/github"
 	"github.com/juju/errors"
 	"golang.org/x/oauth2"
+	git "gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/storage/memory"
 
 	slack "github.com/ashwanthkumar/slack-go-webhook"
 )
@@ -73,7 +76,13 @@ func notifySlack(ctx context.Context, client *github.Client, not github.Notifica
 	attachment.AddField(slack.Field{Title: "Status", Value: status})
 	// Note: red button is Style: "danger"
 	attachment.AddAction(slack.Action{Type: "button", Text: "View Pull Request", Url: "https://github.com/" + strings.Join(parts[1:], "/"), Style: "primary"})
-
+	if status == "Merged" {
+		attachment.AddAction(slack.Action{Type: "button", Text: "Open Setup PR", Url: "https://github.com/" + strings.Join(parts[1:], "/"), Style: "primary"})
+		err = autoSetup(ctx, client, owner, repo)
+		if err != nil {
+			return errors.Trace(err)
+		}
+	}
 	payload := slack.Payload{
 		Text:        "There was some activity on a Pull Request",
 		Username:    "robot",
@@ -115,5 +124,41 @@ func forPRChange(ctx context.Context, username, token string) error {
 		}
 		lastPolled = now
 	}
+	return nil
+}
+
+func autoSetup(ctx context.Context, client *github.Client, owner, repo string) error {
+	rf, _, err := client.Repositories.CreateFork(ctx, owner, repo, nil)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	for {
+		if rf != nil {
+			break
+		}
+	}
+
+	r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+		URL: rf.GetCloneURL(),
+	})
+	headRef, err := r.Head()
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	// Create a new plumbing.HashReference object with the name of the branch
+	// and the hash from the HEAD. The reference name should be a full reference
+	// name and not an abbreviated one, as is used on the git cli.
+	//
+	// For tags we should use `refs/tags/%s` instead of `refs/heads/%s` used
+	// for branches.
+	ref := plumbing.NewHashReference("refs/heads/CodeLingo-Setup", headRef.Hash())
+
+	// The created reference is saved in the storage.
+	err = r.Storer.SetReference(ref)
+	if err != nil {
+		return errors.Trace(err)
+	}
+
 	return nil
 }
